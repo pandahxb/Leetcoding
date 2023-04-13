42c32f50508a64b59f6bdcd296a18e6bc5e99e89
Kaifeng Li <kaifeng-li@outlook.com>
2023-04-13 02:15:01 -0400

    add: 392

---
+++ py/392.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # Empty s is a subsequence of any string
+        if not s:
+            return True
+        # s is not a subsequence if t is empty
+        if not t:
+            return False
+        i = 0
+        for char in t:
+            if s[i] == char:
+                i += 1
+                if i == len(s):
+                    return True
+        return False
