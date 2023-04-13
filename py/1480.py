72198ac45ce23f0955e0de77a3b8c536355372f0
Kaifeng Li <kaifeng-li@outlook.com>
2023-04-13 00:00:26 -0400

    style:

---
+++ py/1480.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = 0
+        result = []
+        for num in nums:
+            running_sum += num
+            result.append(running_sum)
+        return result
