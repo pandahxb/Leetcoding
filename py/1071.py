cba5b9bf6e34f5340cb14011a4cd1113d5e3f2e7
Kaifeng Li <kaifeng-li@outlook.com>
2023-06-06 12:32:14 +0800

    comments: 1071

--- py/1071.py
+++ py/1071.py
@@ -4,6 +4,7 @@ class Solution:
             return ""
         return str1[:self.gcd(len(str1), len(str2))]
 
-    # Euclidean algorithm
+    # Euclidean algorithm: the greatest common divisor of two numbers does not change if the larger number is replaced
+    # by its difference with the smaller number.
     def gcd(self, a, b):
         return a if b == 0 else self.gcd(b, a % b)
